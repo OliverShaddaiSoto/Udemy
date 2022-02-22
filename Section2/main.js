07ac3e93b3d0c9ecabf9cc012c1d6c32ef554e3b
Oliver <oliver-shaddai.soto@atos.net>
2022-02-22 16:17:39 -0600

    Learn the shallow and deep copy and their uses

--- Section2/main.js
+++ Section2/main.js
@@ -31,12 +31,48 @@
 
 
 //Primitive Values
-let sample = 5;
+// let sample = 5;
 
-let copySample = sample;
+// let copySample = sample;
 
-sample = 10;
+// sample = 10;
 
-console.log('sample: ', sample);
+// console.log('sample: ', sample);
 
-console.log('Copy sample: ', copySample);
+// console.log('Copy sample: ', copySample);
+
+//Shallow and deep copy 
+
+const orig = {
+    test: 'test',
+    userDetails: {
+        address: {
+            province: 'Province',
+            country: 'Country'
+        },
+        name: 'myObject'
+    },
+    myFn: () => {},
+    undef: undefined,
+}
+
+const copy = JSON.parse(JSON.stringify(orig));
+
+
+copy.userDetails.name = 'newName';
+
+// console.log('Orig: ', orig);
+// console.log('Copy: ', copy);
+
+const orig2 = ['avocado', 'apple', {name :'strawberry'}];
+
+//const copy2 = orig2.slice();
+
+const copy2 = JSON.parse(JSON.stringify(orig2));
+
+copy2[2].name = 'coconut';
+
+copy2.push('guava');
+
+console.log(orig2);
+console.log(copy2);
