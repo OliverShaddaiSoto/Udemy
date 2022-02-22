77a9fb7314b30e06bf51ba2ed218a2f7f5d3b390
Oliver <oliver-shaddai.soto@atos.net>
2022-02-22 15:09:46 -0600

    Primitive Values

--- Section2/main.js
+++ Section2/main.js
@@ -1,30 +1,42 @@
 //Const, Let & Var
 
-for (let i = 0; i < 3; i++) {
+/*for (let i = 0; i < 3; i++) {
     (function test(o){
         alert(o);
     })(i)
-}
+}*/
 
 //Logical OR & AND 
 // logical OR sample
-const test  = 5 || 6; // 5
+// const test  = 5 || 6; // 5
 
-const sample = 0 || 20; // 20
+// const sample = 0 || 20; // 20
 
 // sample with all falsy values
 // const sample = 0 || false || null; // null
 
 
-console.log(test);
-console.log(sample);
+//console.log(test);
+//console.log(sample);
 
 
 
 
 // logical AND
-const test  = 5 && 6;
-const sample = 0 && false && null;
+// const test  = 5 && 6;
+// const sample = 0 && false && null;
 
-console.log(test); // 6
-console.log(sample); // 0
+// console.log(test); // 6
+// console.log(sample); // 0
+
+
+//Primitive Values
+let sample = 5;
+
+let copySample = sample;
+
+sample = 10;
+
+console.log('sample: ', sample);
+
+console.log('Copy sample: ', copySample);
