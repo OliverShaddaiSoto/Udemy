f5e1ed15d1469f88c5d847d399ae7a1d9f1e2dff
Oliver <oliver-shaddai.soto@atos.net>
2022-02-28 08:56:17 -0600

    Form 	-The form components are similar to Vanilla JavaScript 	-the differences between the reac form props

---
+++ Section3/forms-and-inputs/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { FormElements } from './components/FormElements';
+
+function App() {
+  return (
+    <div className="App">
+      <FormElements />
+    </div>
+  );
+}
+
+export default App;
