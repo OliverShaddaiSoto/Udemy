84c977f676448b25fa1285dafe75954ab720d904
Oliver <oliversotof@gmail.com>
2022-03-09 15:17:42 -0600

    Assigment

---
+++ Section4/assigment/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import MessageEveryday from './components/MessageEveryday';
+
+function App() {
+  return (
+    <div className="App">
+      
+      <MessageEveryday />
+    </div>
+  );
+}
+
+export default App;
